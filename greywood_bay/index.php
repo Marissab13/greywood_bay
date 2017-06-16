ab633b02ba2b2ec53cb1f13e103a47004eda9cff
Marissab13 <marissab13@gmail.com>
2017-06-15 17:17:28 -0700

    Add files via upload

---
+++ greywood_bay/index.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+
+<title>Greywood Bay, LLC</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="description" content="Greywood Bay, LLC is a California based boutique imprint publishing speculative fiction.">
+<style>
+
+body {
+  background: #fff;
+  color: #75787b;
+  font: 18px/1.4 Avenir, AvenirNext, "Avenir Next", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  margin: 0 auto;
+  text-align: center;
+}
+
+* {
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+}
+
+a {
+    color: inherit;
+    text-decoration: none;
+}
+
+a img {
+  border: 0;
+  max-height: 640px
+}
+
+img {
+  max-width: 100%;
+}
+
+p {
+  font-size: 1em;
+  line-height: 1.8;
+}
+
+p strong a {
+    padding: 0 0.5em;
+}
+
+em {
+  font-style: normal;
+}
+
+p em {
+  color: #919599;
+  display: block;
+  font-size: 0.8em;
+}
+
+h1 {
+    font-size: 20px;
+    font-weight: normal;
+    height: 68px;
+    margin: 13px 0 0 0;
+    padding: 48px 0 0 13px;
+}
+
+h1 a {
+    color: #999;
+    -moz-transition:    color 0.63s ease-in;
+    -o-transition:      color 0.63s ease-in;
+    -webkit-transition: color 0.63s ease-in;
+    transition:         color 0.63s ease-in;
+}
+
+h1 a:visited {
+    color: #555;
+}
+
+h1 a:hover,
+h1 a:focus,
+h1 a:active {
+    color: #555;
+}
+
+h1 strong {
+    padding-right: 140px;
+}
+
+section {
+    margin: 4vh 0 12vh;
+}
+
+p {
+    clear: both;
+}
+
+.blurb {
+  color:grey;
+}
+
+ul,
+ul li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+ul li {
+    float: left;
+    margin-right: 10px;
+}
+
+section a:hover,
+section a:focus,
+section a:active {
+    color: #555;
+    text-decoration: none;
+}
+
+nav {
+    margin: 80px auto;
+    padding-bottom: 80px;
+    width: 158px;
+}
+
+li#facebook {
+    margin: 0 10px 80px 10px;
+}
+
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+
+  body {
+  }
+
+  h1 {
+    margin: -90px 0 -4vh;
+  }
+
+  section {
+    margin: 8vh 0 12vh;
+  }
+
+  p {
+    padding: 0 12px;
+  }
+
+  p.blurb {
+    display: none;
+  }
+
+  nav {
+    margin: 4vh auto 1vh;
+    padding-bottom: 80px;
+    width: 158px;
+  }
+}
+
+</style>
+</head>
+<body>
+
+<h1><a href="/">Greywood Bay, LLC</a></h1>
+<div id="portfolio">
+<section id="Forsaken">
+  <a href="http://amzn.to/2rONSHc">
+    <img src="/i/Forsaken White BG.png">
+  </a>
+  <p>
+    Forsaken
+    <em>2011</em>
+  </p>
+  <p class="blurb">
+    NOVEL - Literary Horror // 421 pages.
+  </p>
+</section>
+<section id="A Hollow Dream of Summer's End">
+  <a href="http://amzn.to/2r7BMGF">
+    <img src="/i/ahdose White BG.png" style="display:block;position:relative;margin:auto;">
+  </a>
+  <p>
+    A Hollow Dream - Season One
+    <em>2012</em>
+  </p>
+  <p class="blurb">
+    NOVEL - Literary Horror // 120 pages.
+  </p>
+</section>
+<section id="A Hollow Dream - Eternal Autumn">
+  <a href="http://amzn.to/2sRe1Uk">
+    <img src="/i/AHDoEA white BG.png" style="display:block;position:relative;margin:auto;">
+  </a>
+  <p>
+    A Hollow Dream - Season Two
+    <em>2014</em>
+  </p>
+  <p class="blurb">
+    NOVEL - Horror // Dark Fantasy // 428 pages.
+  </p>
+</section>
+<section id="A Feast of Infinite Rot">
+  <a href="http://amzn.to/2revlWp">
+    <img src="/i/AfoIR White BG.png" style="display:block;position:relative;margin:auto;">
+  </a>
+  <p>
+    A Feast of Infinite Rot
+    <em>2014</em>
+  </p>
+  <p class="blurb">
+    Novella - Horror // Dark Fantasy // 110 pages.
+  </p>
+</section>
+
+
+<nav>
+  <ul>
+  </ul>
+</nav>
+
+<script>
+// retina.js, a high-resolution image swapper (http://retinajs.com), v1.3.0
+!function(){function a(){}function b(a){return f.retinaImageSuffix+a}function c(a,c){if(this.path=a||"","undefined"!=typeof c&&null!==c)this.at_2x_path=c,this.perform_check=!1;else{if(void 0!==document.createElement){var d=document.createElement("a");d.href=this.path,d.pathname=d.pathname.replace(g,b),this.at_2x_path=d.href}else{var e=this.path.split("?");e[0]=e[0].replace(g,b),this.at_2x_path=e.join("?")}this.perform_check=!0}}function d(a){this.el=a,this.path=new c(this.el.getAttribute("src"),this.el.getAttribute("data-at2x"));var b=this;this.path.check_2x_variant(function(a){a&&b.swap()})}var e="undefined"==typeof exports?window:exports,f={retinaImageSuffix:"@2x",check_mime_type:!0,force_original_dimensions:!0};e.Retina=a,a.configure=function(a){null===a&&(a={});for(var b in a)a.hasOwnProperty(b)&&(f[b]=a[b])},a.init=function(a){null===a&&(a=e);var b=a.onload||function(){};a.onload=function(){var a,c,e=document.getElementsByTagName("img"),f=[];for(a=0;a<e.length;a+=1)c=e[a],c.getAttributeNode("data-no-retina")||f.push(new d(c));b()}},a.isRetina=function(){var a="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-resolution: 1.5dppx)";return e.devicePixelRatio>1?!0:e.matchMedia&&e.matchMedia(a).matches?!0:!1};var g=/\.\w+$/;e.RetinaImagePath=c,c.confirmed_paths=[],c.prototype.is_external=function(){return!(!this.path.match(/^https?\:/i)||this.path.match("//"+document.domain))},c.prototype.check_2x_variant=function(a){var b,d=this;return this.is_external()?a(!1):this.perform_check||"undefined"==typeof this.at_2x_path||null===this.at_2x_path?this.at_2x_path in c.confirmed_paths?a(!0):(b=new XMLHttpRequest,b.open("HEAD",this.at_2x_path),b.onreadystatechange=function(){if(4!==b.readyState)return a(!1);if(b.status>=200&&b.status<=399){if(f.check_mime_type){var e=b.getResponseHeader("Content-Type");if(null===e||!e.match(/^image/i))return a(!1)}return c.confirmed_paths.push(d.at_2x_path),a(!0)}return a(!1)},b.send(),void 0):a(!0)},e.RetinaImage=d,d.prototype.swap=function(a){function b(){c.el.complete?(f.force_original_dimensions&&(c.el.setAttribute("width",c.el.offsetWidth),c.el.setAttribute("height",c.el.offsetHeight)),c.el.setAttribute("src",a)):setTimeout(b,5)}"undefined"==typeof a&&(a=this.path.at_2x_path);var c=this;b()},a.isRetina()&&a.init(e)}();
+</script>
+</body>
